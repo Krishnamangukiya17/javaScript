59f28d905bfbff2a48aaf5b25f27fc242fdd1b8a
Krishnamangukiya17 <krishnamangukiya17@gmail.com>
2023-05-19 06:59:51 +0530

    oprator
    oprator

---
+++ 05_operatorAssignmentC.js
@@ -0,0 +1,34 @@
+
+console.log("----------------------------------------------");
+console.log("         TCS Interview Eligibility            ");
+console.log("----------------------------------------------");
+// var graduaction=70;
+// var hsc =80;
+// var ssc=90;
+
+// var krishnaScore=function (gradScore){
+// var result=graduaction>=80 && hsc>=86 && ssc>=90 ?" krishna congratilatons..you are eligible for interview ":" sorry....you not eligible for interview"
+// result`${result}`;
+// }
+// var result=krishnaScore(80,86,90);
+// console.log( result);
+var graduaction =80
+var hsc = 86
+var ssc = 90;
+var result =graduaction>=70 && hsc>=80 && ssc >=90 ? " krishna congratilatons..you are eligible for interview" : " sorry...you not eligible for interview";
+console.log(`${result}`);
+
+console.log("----------------------------------------------");
+var graduaction =70;
+var hsc = 65;
+var ssc = 55;
+var result =graduaction>=70 && hsc>=80 && ssc >=90 ? " Zalak congratilatons..you are eligible for interview" : " sorry...you not eligible for interview";
+console.log(`${result}`);
+
+console.log("----------------------------------------------");
+var graduaction =60;
+var hsc = 79;
+var ssc = 88;
+var result =graduaction>=70 && hsc>=80 && ssc >=90 ? " Hardi congratilatons..you are eligible for interview" : " sorry...you not eligible for interview";
+console.log(`${result}`);
+console.log("----------------------------------------------");
