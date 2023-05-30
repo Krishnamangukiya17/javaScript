bd12addd4b0325a614ecbf7a8800e83970c296d5
Krishnamangukiya17 <krishnamangukiya17@gmail.com>
2023-05-30 22:04:45 +0530

    object assignment
    object assignment

---
+++ 11_object_assignE.js
@@ -0,0 +1,48 @@
+let sbi = {
+  name: "State bank of india",
+  location: "A-43 maruti chambers,shivalik plaza",
+  accountNumber: "845952058445",
+  ifsc: "SBI4857591",
+  interestRate: "6.50%",
+};
+
+let axisBank = {
+  name: "Axis Bank",
+  location: "A-45 maruti chambers,om plaza",
+  accountNumber: "8458151458445",
+  ifsc: "AXI4857591",
+  interestRate: "6.50%",
+};
+
+let hdfc = {
+  name: "HDFC",
+  location: "A-43 maruti chambers,Ahemdabad",
+  accountNumber: "000000058445",
+  ifsc: "HDF4857591",
+  interestRate: "6.50%",
+};
+
+let yesBank = {
+  name: "Yes Bank",
+  location: "c-43 maruti chambers plaza",
+  accountNumber: "00000058445",
+  ifsc: "YES4857591",
+  interestRate: "6.50%",
+};
+
+console.log(
+  "---------------------------------------------------------SBI------------------------------------------------------------------------"
+);
+console.log(sbi);
+console.log(
+  "---------------------------------------------------------AXIS BANK------------------------------------------------------------------------"
+);
+console.log(axisBank);
+console.log(
+  "---------------------------------------------------------HDFC BANK------------------------------------------------------------------------"
+);
+console.log(hdfc);
+console.log(
+  "---------------------------------------------------------YES BANK------------------------------------------------------------------------"
+);
+console.log(yesBank);
