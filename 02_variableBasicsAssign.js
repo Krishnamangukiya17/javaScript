8cedfdbc91c8acfbd1ce78fe7e07d63781a20226
Krishnamangukiya17 <krishnamangukiya17@gmail.com>
2023-06-06 13:45:57 +0530

    typeof
    typeof

--- 02_variableBasicsAssign.js
+++ 02_variableBasicsAssign.js
@@ -1,20 +1,52 @@
 
-console.log(".......(1)NUMBER......");
+console.log(".........................(1)NUMBER.......................");
 
 
-var Number=411056;
-console.log("Number:",Number);
+var number=411056;
+console.log("Number:",number);
+var typeofNumber =typeof number;
+console.log("Numbers type is ->",typeofNumber);
+
  var pin=45469;
  console.log("pin:", pin);
+var typeOfPin=typeof pin;
+console.log("Pin type is : ->",typeOfPin);
+
  var mobile=2356789;
  console.log("mobile:",mobile);
- console.log(".......(2)String......");
- var firstName="Krishna mangukiya";
+ var typeofMobile = typeof mobile;
+ console.log("Mobile number type is: ->",typeofMobile);
+
+ console.log(".........................(2)String........................");
+ var firstName="Krishna ";
  console.log(firstName);
  var typeOfFirstName = typeof firstName;
- console.log("Type of variable -> firstName: ", typeOfFirstName);
- console.log(".......(3)boolean......");
+ console.log("Type of variable is: -> ", typeOfFirstName);
+
+ var lastName="mangukiya";
+ console.log("Last Name:",lastName);
+ var typeOfLastName = typeof lastName;
+ console.log("Type of variable is: -> ", typeOfLastName);
+
+ var schoolName="SoS";
+ console.log("School Name: ",schoolName);
+ var typeOfSchoolName = typeof  schoolName;
+ console.log("Type of variable is: -> ",  typeOfSchoolName);
+
+
+ console.log("........................(3)Boolean........................");
  
 var isSoftwareEngineer=false;
-console.log
+console.log( "Is Software Engineer",isSoftwareEngineer );
+var typeOf= typeof isSoftwareEngineer;
+console.log("Type of variable is: -> ", typeOf);
 
+var isIndian=true;
+console.log("Is Indian:",isIndian);
+typeOf=typeof isIndian;
+console.log("Type of variable is: -> ",typeOf);
+
+var isMarried=true;
+console.log("Is MArried:",isMarried);
+typeOf=typeof isMarried;
+console.log("Type of variable is: -> ",typeOf);
