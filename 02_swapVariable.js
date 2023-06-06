38193a93e715b60a61a7692521f2aa5be76ec2f2
Krishnamangukiya17 <krishnamangukiya17@gmail.com>
2023-06-06 14:16:07 +0530

    swape
    swape

--- 02_swapVariable.js
+++ 02_swapVariable.js
@@ -6,7 +6,7 @@ var sweety=100;
 var cutie=200;
 console.log("----------Before swap----------");
 console.log("------------------------------");
-console.log("sweety:",100,"cutie:",200);  
+console.log("sweety:",sweety,"cutie:",cutie);  
 console.log("----------After swap----------");
 console.log("------------------------------");
 var temp=sweety;
@@ -20,7 +20,7 @@ var num2=200;
 var num3=300;
 console.log("----------Before swap----------")
 console.log("------------------------------")
-console.log("num1:",100,"num2:",200, "num3:",300,);  
+console.log("num1:",num1,"num2:",num2, "num3:",num3,);  
 console.log("----------After swap----------")
 console.log("------------------------------")
 num1=num2;
